5890d8f863fb9b5f874bda5b3796bf5535436b6f
wanda <2076639110@qq.com>
2021-09-10 22:51:37 +0800

    星空

---
+++ 书本例题.py
@@ -0,0 +1,7 @@
+#本文件不将保存什么东西
+#print(round(18.67,-1),round(123.84,-2))
+'''num=int(input("输入一个整数："))
+while(num!=0):
+    print(num%10,end='')
+    num=num//10'''
+
---
+++ 命令行的使用.py
@@ -0,0 +1,6 @@
+#要在运行中使用，采用访问文件地址的方法
+import sys
+a=int(sys.argv[1])
+b=int(sys.argv[2])
+sum=a+b
+print(a,'+',b,'=',sum)
