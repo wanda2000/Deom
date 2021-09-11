563024874cbf3f2ceb29e0d05fdb98eefe1f0a39
wanda <2076639110@qq.com>
2021-09-11 12:09:36 +0800

    星空

--- 书本例题.py
+++ 书本例题.py
@@ -3,5 +3,46 @@
 '''num=int(input("输入一个整数："))
 while(num!=0):
     print(num%10,end='')
-    num=num//10'''
-
+    num=num//10
+#计算落体100米的距离
+i=100
+meter=100
+h=0
+j=0
+while(j<=(2**10)):
+    j=2**j
+    meter+=i/j
+    j+=1
+print("%f,%f",(meter,i/(2**10)))
+#吃桃数字
+a=1
+b=[1]
+i=7
+while i>0 :
+    j=2*(a+1)
+    b.append(j)
+    i-=1
+    a=j
+print(b[0])
+print(b[1])
+print(b[2])
+print(b[3])
+print(b[4])
+print(b[5])
+print(b[6])
+print(b[7])
+#sn=1+11+111+1111+……………
+import random
+n=random.randint(1,11)
+t1=1
+i=1
+sn=1
+b=[1]
+while i<=n :
+    for j in range(i-1,i):
+        t=b[j]*10+1
+        b.append(t)
+        sn+=t
+        i+=1
+print("n={0},sn={1}".format(n,sn))
+'''
