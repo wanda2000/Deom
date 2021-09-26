48ac23e670b027dc8bf290954ac8dbea2259b9ae
wanda <2076639110@qq.com>
2021-09-26 11:08:39 +0800

    星空

--- 百战.py
+++ 百战.py
@@ -18,8 +18,130 @@
 # #         turtle.goto(i,j)
 #
 # x.exitonclick()
-a='abcdef\
-ghijkmn\
-opqrst\
-uvwxyz'
-print(a)
+
+# #replace 返回一个新对象，并没有改变字符串
+# a='abcdef\
+# ghijkmn\
+# opqrst\
+# uvwxyz'
+# print(id(a))
+# print(type(a))
+# print(a)
+# a=a.replace('a','熊')
+# print(id(a))
+# print(type(a))
+# print(a)
+
+#变量未被声明
+# b='i am a good boy'
+# print(b)
+# del b
+# print(b)
+
+#int 格式不能转字符型的浮点数
+# print(int(0x123))
+# i = 0o123
+# j = 0b01010010
+# print(int(i),int(j))
+# b=int(9.9)
+# print(b)
+# c = int(False)
+# print(c)
+# d = int ("456.78")
+# print(d)
+
+# a = 1000
+# b = 1000
+# print(a is b,id(a),id(b))
+# print(a == b)
+# y=5
+# while 3<y<9:
+#     print("1")
+#     y+=1
+
+# a = ~0b10101010
+# print(bin(a))
+'''
+a=[1,3,5,7,7,7,5,3]
+# for i in range(len(a)-1,-1,-1):
+#     if a[i] == 7:
+#         a.remove(7)
+# print(a)
+
+# for i in a[:]:
+#      if i == 7:
+#         a.remove(7)
+# print(a)
+
+# for i in a:
+#     if i == 7:
+#         a.remove(7)
+# # print(a)
+'''
+# list_1=[]
+# for i in a:
+#     if i not in list_1:
+#         list_1.append(i)
+# print(list_1)
+# a='abcdefghijklmnopqrstuvwxyz'
+# a= a.replace('a','熊')
+# print(a)
+
+# for i in range(len(a)-1,-1,-1):
+#     if a[i] == 7:
+#         a.pop(i)
+#         print(i)
+# print(a)
+# print(len(a))
+# print(len(a))
+
+# a = 'abcdefghijklmnopqrstuvwxyz'
+# a = a.replace('a',"xiong")
+# print(a)
+#
+# a = 'to be or not to be , this is a question!'
+# b = a.split('be')
+# print(a)
+# print(b)
+
+# import time
+# time_01 = time.time()
+# a=''
+# for i in range(100000):
+#     x='xiong'
+#     a+=x
+# time_02 = time.time()
+# print(time_02-time_01)
+#
+# time_03 = time.time()
+# c=[]
+# for j in range(100000):
+#     c.append('xiong')
+# d=''.join(c)
+# time_04 = time.time()
+# print(time_04-time_03)
+
+import turtle
+
+t = turtle.Pen()
+color_01=("orange","gold","pink","brown","grey","purple","blue"\
+        ,"yellow","green","black","white","red","gold","pink","brown","grey","purple","blue","blue")
+x = [-200,100]
+y = [-20,-80]
+t.speed(10)
+for i in range(0,19):
+    t.penup()
+    t.color(color_01[i])
+    t.goto(x[0],100-10*i)
+    t.pendown()
+    t.goto(y[0],100-10*i)
+
+for j in range(0,19):
+    t.penup()
+    t.color(color_01[j])
+    t.goto(x[0]+10*j,x[1])
+    t.pendown()
+    t.goto(x[0]+10*j,y[1])
+
+t.hideturtle()
+turtle.done()
